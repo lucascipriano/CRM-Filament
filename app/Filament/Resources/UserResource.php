9b07aeead0744cd211767be2a5d51ee785f21c65
Lucas Cipriano <lucasncborges@gmail.com>
2024-11-22 20:41:03 -0300

    ajust

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -18,7 +18,6 @@ use Illuminate\Support\Facades\Hash;
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
-
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
     protected static ?string $navigationGroup = 'Adminstração';
 
