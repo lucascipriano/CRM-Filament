d2b6d96b970683a33875d3d0bad6a13ef6b154c1
Lucas Cipriano <lucasncborges@gmail.com>
2024-11-25 13:43:38 -0300

    Remover o CRM do titulo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->brandName('Egbé Àyé - CRM')
+            ->brandName('Egbé Àyé')
             ->colors([
                 'primary' => Color::Rose,
             ])
